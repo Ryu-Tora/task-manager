f381774c368c0e493fd145901320ad7d6af1b30b
Adam Halka <galkaadam4@gmail.com>
2025-11-27 06:39:40 +0100

    added CRUD operation to Task in views

--- manager/views.py
+++ manager/views.py
@@ -58,6 +58,22 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 10
 
 
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = "manager:task-list"
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("manager:task-list")
+
+
 class PositionListView(LoginRequiredMixin, generic.ListView):
     model = Position
     paginate_by = 10
@@ -66,4 +82,3 @@ class PositionListView(LoginRequiredMixin, generic.ListView):
 class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
     paginate_by = 10
-
