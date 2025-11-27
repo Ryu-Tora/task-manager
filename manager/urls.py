f597f74a4cbf02bd69be60801f2205530e7c158a
Adam Halka <galkaadam4@gmail.com>
2025-11-27 06:28:46 +0100

    in manager/urls created urls for lists

--- manager/urls.py
+++ manager/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
 
-from manager.views import index
+from manager.views import index, PositionListView, TaskListView, TaskTypeListView, WorkerListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list"
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list"
+    ),
+    path("task_types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list"
+    ),
+    path("workers/",
+        WorkerListView.as_view(),
+        name="worker-list"
+    ),
     ]
