ac99cd52ce92fc099e99d102ac65a94d15fa32dc
Adam Halka <galkaadam4@gmail.com>
2025-12-12 10:05:22 +0100

    Final change in urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -1,27 +1,68 @@
+from django.conf.urls.static import static
 from django.urls import path
 
-from manager.views import index, PositionListView, TaskListView, TaskTypeListView, WorkerListView
-
-urlpatterns = [
-    path("", index, name="index"),
-    path(
-        "positions/",
-        PositionListView.as_view(),
-        name="position-list"
-    ),
-    path(
-        "tasks/",
-        TaskListView.as_view(),
-        name="task-list"
-    ),
-    path("types/",
-        TaskTypeListView.as_view(),
-        name="task-type-list"
-    ),
-    path("workers/",
-        WorkerListView.as_view(),
-        name="worker-list"
-    ),
-]
+from task_manager import settings
+from . import views
 
 app_name = "manager"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # Tasks
+    path("tasks/", views.TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task-delete"),
+    # Task Types
+    path("task-types/", views.TaskTypeListView.as_view(), name="task-type-list"),
+    path(
+        "task-types/create/",
+        views.TaskTypeCreateView.as_view(),
+        name="task-type-create",
+    ),
+    path(
+        "task-types/<int:pk>/update/",
+        views.TaskTypeUpdateView.as_view(),
+        name="task-type-update",
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        views.TaskTypeDeleteView.as_view(),
+        name="task-type-delete",
+    ),
+    # Positions
+    path("positions/", views.PositionListView.as_view(), name="position-list"),
+    path(
+        "positions/<int:pk>/",
+        views.PositionDetailView.as_view(),
+        name="position-detail",
+    ),
+    path(
+        "positions/create/", views.PositionCreateView.as_view(), name="position-create"
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        views.PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        views.PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
+    # Workers
+    path("workers/", views.WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", views.WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/create/", views.WorkerCreateView.as_view(), name="worker-create"),
+    path(
+        "workers/<int:pk>/update/",
+        views.WorkerUpdateView.as_view(),
+        name="worker-update",
+    ),
+    path(
+        "workers/<int:pk>/delete/",
+        views.WorkerDeleteView.as_view(),
+        name="worker-delete",
+    ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
