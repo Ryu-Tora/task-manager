94229d28af80d243b06fa43b8c54f3bbcfa93433
Adam Halka <galkaadam4@gmail.com>
2025-12-18 14:04:17 +0100

    changed debug settings in setting.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,9 +26,9 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
